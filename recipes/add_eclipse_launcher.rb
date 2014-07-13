e52c6cfdbb8ef293e4346f7b88fdf4a04eb0e0b4
Shigemi ISHIDA <ishida+devel@f.ait.kyushu-u.ac.jp>
2014-07-13 20:45:41 +0900

    Bug fix on the recipe add_eclipse_lancher: * Modify file owner and group. * Modify comments. * Modify variable handling in template.

--- recipes/add_eclipse_launcher.rb
+++ recipes/add_eclipse_launcher.rb
@@ -1,23 +1,15 @@
 #
 # Cookbook Name:: xfce4
-# Recipe:: default
+# Recipe:: add_eclipse_launcher
 #
 # Copyright (c) 2014 Shigemi ISHIDA
 #
 # This software is released under the MIT License.
 # http://opensource.org/licenses/mit-license.php
 
-conf = node['xfce4']['eclipse']
-
-version   = conf['version']
-app_name  = conf['app_name']
-comment   = conf['comment']
-bin_path  = conf['bin_path']
-icon_path = conf['icon_path']
-
 template "/usr/share/applications/eclipse.desktop" do
   source "eclipse.desktop.erb"
   mode 0644
-  user root
-  group root
+  owner "root"
+  group "root"
 end
